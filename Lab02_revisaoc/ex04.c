8dbe11b9cd1ac082d54ec393bdb75a31f96682f5
matheus-mvk <matheusmhvs1@gmail.com>
2022-05-22 16:34:32 -0300

    ex04 concluido

---
+++ Lab02_revisaoc/ex04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    
+    int dados[] = {1, 2, 3, 4, 5, 22, 1, 55, 10, 9};
+    for(int i = 0; i < 10; i++){
+        printf("%d  ", dados[i]);
+    }
+
+    return 0;
+}
