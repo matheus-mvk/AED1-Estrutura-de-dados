927a2ea853de6926d8360a20c5c2867133376948
matheus-mvk <matheusmhvs1@gmail.com>
2022-05-22 16:10:03 -0300

    ex02 concluido

---
+++ Lab02_revisaoc/ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>>
+
+int main() 
+{
+    char nome[] = "Jose Augusto";
+    for(int i = 0; i < strlen(nome); i++){
+        printf("%c", nome[i]);
+    }
+    
+    return 0;
+}
