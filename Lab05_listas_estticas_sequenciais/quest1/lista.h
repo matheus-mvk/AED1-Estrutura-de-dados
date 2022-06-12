3b2664044ab32f5d1b1b37384c34a21f9dcd8b7f
matheus-mvk <matheusmhvs1@gmail.com>
2022-06-12 15:34:24 -0300

    lab05 iniciado, questão 1 finalizada

---
+++ Lab05_listas_estticas_sequenciais/quest1/lista.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct lista lista;
+
+lista* criar_lista();
+int lista_vazia(lista *lst);
+int lista_cheia(lista *lst);
+int insere_elem(lista *lst, int elem);
+int remove_elem(lista *lst, int elem);
+void obtem_valor_elem(lista *lst);
